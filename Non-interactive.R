938db7488d1edc365c149516037bae9b3b7f02ac
Will Robson <W.robson1@newcastle.ac.uk>
2021-05-05 18:28:28 +0100

    Chaned some aspects of the code for work. Dissolved code with viewshed as does not work effecttively

--- Non-interactive.R
+++ Non-interactive.R
@@ -8,8 +8,96 @@ library(mapview)
 library(sf)
 options("rgdal_show_exportToProj4_warnings"="none")
 library(raster)
+library(rgbif)
+library(ggplot2)
+library(dplyr)
+library(rgbif)
+library(BIRDS)
+
+#NBN data ----
+
+#Cuckoos first
+cuckoo_records <- read.csv("NBN/Cuckoo_records.csv")
+
+#Plotting to observe trends
+ggplot(cuckoo_records, aes(x=year.processed)) +
+  geom_histogram()
+
+cuckoo_records_per_yr <- cuckoo_records %>% 
+  group_by(year.processed) %>% 
+  summarise(count_per_year = n())
+
+ggplot(cuckoo_records_per_yr, aes(x = year.processed, y=count_per_year)) +
+  geom_line() + xlab("Years") + ylab("Birds observed")
+
+#Hen harriers
+henharriers_records <- read.csv("NBN/Hen_harriers_records.csv")
+
+#observing trends
+ggplot(henharriers_records, aes(x=year.processed)) +
+  geom_histogram()
+
+henharriers_records_per_yr <- henharriers_records %>% 
+  group_by(year.processed) %>% 
+  summarise(count_per_year = n())
+
+ggplot(henharriers_records_per_yr, aes(x = year.processed, y=count_per_year)) +
+  geom_line() + xlab("Years") + ylab("Birds observed")
+
+#Long eared owl data
+longearedowl_records <- read.csv("NBN/Longearedowl_records.csv")
+
+#observing trends
+ggplot(longearedowl_records, aes(x=year.processed)) +
+  geom_histogram()
+
+longearedowl_records_per_yr <- longearedowl_records %>% 
+  group_by(year.processed) %>% 
+  summarise(count_per_year = n())
+
+ggplot(longearedowl_records_per_yr, aes(x = year.processed, y=count_per_year)) +
+  geom_line() + xlab("Years") + ylab("Birds observed")
+
+#Now joining all datapoints together
+bird_plot <- leaflet() %>%
+  addTiles(group = "OSM (default)") %>% 
+  addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>% 
+  addCircleMarkers(cuckoo_records$decimalLongitude.processed, cuckoo_records$decimalLatitude.processed, label = cuckoo_records$scientificName.processed, 
+                   labelOptions = labelOptions(interactive = "TRUE"),
+                   radius = 2, fillOpacity = 0.5, opacity = 0.5, col="red", popup = cuckoo_records$scientificNameprocessed) %>%
+  addCircleMarkers(henharriers_records$decimalLongitude.processed, henharriers_records$decimalLatitude.processed, label = henharriers_records$scientificName.processed, 
+                   labelOptions = labelOptions(interactive = "TRUE"),
+                   radius = 2, fillOpacity = 0.5, opacity = 0.5, col="blue", popup = henharriers_records$scientificName.processed) %>%
+  addCircleMarkers(longearedowl_records$decimalLongitude.processed, longearedowl_records$decimalLatitude.processed, label = longearedowl_records$scientificName.processed,
+                   labelOptions = labelOptions(interactive = "TRUE"),
+                   radius = 2, fillOpacity = 0.5, opacity = 0.5, col="yellow", popup = longearedowl_records$scientificNameprocessed) %>%  
+                      addLayersControl(
+                     baseGroups = c("OSM (default)", "Satellite"), 
+                     overlayGroups = c("Cuckoo", "Hen Harrier", "Long eared owl"),
+                     options = layersControlOptions(collapsed = TRUE)) %>% 
+                     addLegend("bottomright", colors = "reds", 
+                    opacity=1, labels="Cuckoo", "Hen harrier", "Long eared owl")
+  
+
+bird_plot
 
 #Testing to see if leaflet will read in vectors ----
+
+settlement <- st_read("www/cumbria_settlements.shp")
+settlemnt_ll <- st_transform(settlement,crs=ll_crs)
+
+settlement_view <- leaflet() %>% 
+  addTiles(group = "OSM (default)") %>% 
+  addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>% 
+  addFeatures(settlemnt_ll) %>% 
+  addLayersControl(
+    baseGroups = c("OSM (default)", "Satellite"), 
+    overlayGroups = c("Settlements"),
+    options = layersControlOptions(collapsed = TRUE)
+  )
+
+settlement_view
+
 lakes <- st_read("www/cumbria_lakes.shp")
 lakes_ll <- st_transform(lakes,crs=ll_crs)
 
@@ -39,6 +127,8 @@ addTiles(group = "OSM (default)") %>%
 
 elevation_view #This also works 
 
+
+
 #Plotting the raster elevation maps ----
 
 # Import raster elevation data Ordnance Survey projection
@@ -59,14 +149,6 @@ elevation_ll <- projectRaster(elevation, crs=ll_crs)
 
 mapview(elevation_ll)
 
-#Creating contours from raster DTM ----
-
-elevation_contours <- rasterToContour(elevation) %>% st_as_sf()
-plot(elevation, col=terrain.colors(30)) #Remember to add this code each time to alter colouring patterns
-plot(elevation_contours, add=TRUE)
-
-mapview(elevation_contours, add=TRUE) #Interactive view again
-
 #Add DTM data to site data (wind turbines) ----
 
 wind_turbines <- st_read("www/windfarm.shp")
@@ -80,23 +162,9 @@ plot(wind_turbines, add=TRUE) #Viewing on map, appears as circle around South We
 
 mapview(st_transform(wind_turbines, 4326)) #Viewing on interactive map
 
-#Calculating slope and aspect aspect ----
-
-slope  <- terrain(elevation, unit="degrees") # defaults to slope
-aspect <- terrain(elevation, opt="aspect", unit="degrees")
-
-plot(slope, col=terrain.colors(30))
-plot(aspect, col=terrain.colors(30))
-
-#Add slope and aspect values to wind turbines attributes ----
-#(This may not be needed as windfarm already has slope and aspect)
-
-wind_turbines$slope <- extract(slope, wind_turbines)
-wind_turbines$aspect <- extract(aspect, wind_turbines)
-
 wind_turbines #viewing the changes
 
-#Create a viewshed ----
+#Create a viewshed (scrapped) ----
 
 source("LOS.R")
 
@@ -110,10 +178,4 @@ windfarm_5 <- dplyr::filter(wind_turbines, Turb_ID == "5")
 # Change to coarser 500m elevation map for speed
 elevation500m <- aggregate(elevation, fact=10) # fact=10 is the number of cells aggregated together
 
-# Extract just the geometry for a single mast, and pass to viewshed function.
-# Adding a 5km maximum radius
-# Takes 1 to 2 minutes to run viewshed depending on your PC
-windfarm_geom <- st_geometry(windfarm_5)[[1]]
-cumbria_vw <- viewshed(dem=elevation500m, windfarm=windfarm_geom,
-                          h1=1.5, h2=50, radius=2000) #Fix this one as broken 
 
